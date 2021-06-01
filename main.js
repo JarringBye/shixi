ae5e9ba558be4b05086e2dd0969c8cd9352ec1eb
Binary2101 <1433914590@qq.com>
2021-06-01 22:35:02 +0800

    tongbu

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let io = require('socket.io')(server, {cors: true});
 var connid="";
 app.all("*", function (req, res, next) {
     //设置允许跨域的域名，*代表允许任意域名跨域
-    res.header("Access-Control-Allow-Origin", "http://lalila.top:5716");
+    res.header("Access-Control-Allow-Origin", "http://www.lalila.top:5716");
     //允许的header类型
     res.header("Access-Control-Allow-Headers", "content-type");
     //跨域允许的请求方式
